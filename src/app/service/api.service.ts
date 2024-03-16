657e3002ffc9174515bf4c1778ee2c02e78694a3
Marlon Leoner <marlon.leoner@gmail.com>
2024-03-16 19:33:40 -0300

    players model add to api service

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -3,13 +3,16 @@ import { Injectable, inject } from '@angular/core';
 import { QueryObserverResult, injectMutation, injectQuery, injectQueryClient } from '@ngneat/query';
 import { Result } from '@ngneat/query/lib/types';
 
-import { ITeamResponse } from '../types/api';
+import { IPlayerResponse, ITeamResponse } from '../types/api';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ApiService {
     private baseUrl: string = 'http://localhost:6779/api/';
+
+    private teamsKey = 'teams';
+    private playersKey = 'players';
 
     private http = inject(HttpClient);
 
@@ -25,27 +28,27 @@ export class ApiService {
 
     getTeams = (): Result<QueryObserverResult<ITeamResponse[], Error>> => {
         return this.query({
-            queryKey: ['teams'] as const,
+            queryKey: [this.teamsKey] as const,
             queryFn: () => {
-                return this.http.get<ITeamResponse[]>(`${this.baseUrl}teams`);
+                return this.http.get<ITeamResponse[]>(`${this.baseUrl}${this.teamsKey}`);
             }
         });
     };
 
     private createTeam = () => {
         return this.mutation({
-            mutationFn: (team: ITeam) => this.http.post<ITeamResponse>(`${this.baseUrl}teams`, team),
+            mutationFn: (team: ITeam) => this.http.post<ITeamResponse>(`${this.baseUrl}${this.teamsKey}`, team),
             onSuccess: () => {
-                this.client.invalidateQueries({ queryKey: ['teams'] });
+                this.client.invalidateQueries({ queryKey: [this.teamsKey] });
             }
         });
     };
 
     private updateTeam = () => {
         return this.mutation({
-            mutationFn: (team: ITeam) => this.http.put<ITeamResponse>(`${this.baseUrl}teams`, team),
+            mutationFn: (team: ITeam) => this.http.put<ITeamResponse>(`${this.baseUrl}${this.teamsKey}`, team),
             onSuccess: () => {
-                this.client.invalidateQueries({ queryKey: ['teams'] });
+                this.client.invalidateQueries({ queryKey: [this.teamsKey] });
             }
         });
     };
@@ -53,5 +56,37 @@ export class ApiService {
     saveTeam = (isEditing: boolean) => {
         if (isEditing) return this.updateTeam();
         return this.createTeam();
+    };
+
+    getPlayers = (): Result<QueryObserverResult<IPlayerResponse[], Error>> => {
+        return this.query({
+            queryKey: [this.playersKey] as const,
+            queryFn: () => {
+                return this.http.get<IPlayerResponse[]>(`${this.baseUrl}${this.playersKey}`);
+            }
+        });
+    };
+
+    private createPlayer = () => {
+        return this.mutation({
+            mutationFn: (player: any) => this.http.post<ITeamResponse>(`${this.baseUrl}${this.playersKey}`, player),
+            onSuccess: () => {
+                this.client.invalidateQueries({ queryKey: [this.playersKey] });
+            }
+        });
+    };
+
+    private updatePlayer = () => {
+        return this.mutation({
+            mutationFn: (player: any) => this.http.put<ITeamResponse>(`${this.baseUrl}${this.playersKey}`, player),
+            onSuccess: () => {
+                this.client.invalidateQueries({ queryKey: [this.playersKey] });
+            }
+        });
+    };
+
+    savePlayer = (isEditing: boolean) => {
+        if (isEditing) return this.updatePlayer();
+        return this.createPlayer();
     };
 }
--- src/app/types/api.d.ts
+++ src/app/types/api.d.ts
@@ -5,3 +5,14 @@ export interface ITeamResponse {
     tag?: string;
     country?: string;
 }
+
+export interface IPlayerResponse {
+    id: string;
+    nickname: string;
+    steamId: string;
+    firstName: string;
+    lastName: string;
+    avatar: string;
+    country: string;
+    team: ITeamResponse;
+}
