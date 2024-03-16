67b82fb8bb503a6854991acfbaff7aada374ba1d
Marlon Leoner <marlon.leoner@gmail.com>
2024-03-16 14:54:28 -0300

    teams page created

---
+++ src/app/pages/teams/teams.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'genieshm-teams',
+    standalone: true,
+    imports: [],
+    templateUrl: './teams.page.html',
+    styleUrl: './teams.page.css'
+})
+export class TeamsPage {}
