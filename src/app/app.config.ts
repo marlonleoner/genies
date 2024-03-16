9271bdcb0816b71958ff2f0ccea135b0fc8ea295
Marlon Leoner <marlon.leoner@gmail.com>
2024-03-16 19:00:21 -0300

    ngneat query add to project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,4 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { LucideAngularModule, icons } from 'lucide-angular';
@@ -5,5 +6,9 @@ import { LucideAngularModule, icons } from 'lucide-angular';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes, withHashLocation()), importProvidersFrom(LucideAngularModule.pick(icons))]
+    providers: [
+        provideRouter(routes, withHashLocation()),
+        provideHttpClient(),
+        importProvidersFrom(LucideAngularModule.pick(icons))
+    ]
 };
