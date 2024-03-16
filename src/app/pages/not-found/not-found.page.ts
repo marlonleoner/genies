e798bb23648cd6183280a97531b2969abf904473
Marlon Leoner <marlon.leoner@gmail.com>
2024-03-16 14:54:17 -0300

    not found page created

---
+++ src/app/pages/not-found/not-found.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'genieshm-not-found',
+    standalone: true,
+    imports: [],
+    templateUrl: './not-found.page.html',
+    styleUrl: './not-found.page.css'
+})
+export class NotFoundPage {}
