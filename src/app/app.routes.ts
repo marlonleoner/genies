e4d9aa09fbca71df1bcac0d5417d5971055a9f32
Marlon Leoner <marlon.leoner@gmail.com>
2024-03-16 19:33:50 -0300

    players page created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { NotFoundPage } from './pages/not-found/not-found.page';
+import { PlayersPage } from './pages/players/players.page';
 import { TeamsPage } from './pages/teams/teams.page';
 
 export const routes: Routes = [
     { path: 'teams', component: TeamsPage },
+    { path: 'players', component: PlayersPage },
     { path: '**', component: NotFoundPage }
 ];
---
+++ src/app/pages/players/players.page.ts
@@ -0,0 +1,107 @@
+import { CommonModule } from '@angular/common';
+import { Component, Signal, inject } from '@angular/core';
+import { QueryObserverResult } from '@ngneat/query';
+import { LucideAngularModule } from 'lucide-angular';
+
+import { GHMButtonComponent } from '../../components/button/button.component';
+import { GHMInputComponent } from '../../components/input/input.component';
+import { GHMModalComponent } from '../../components/modal/modal.component';
+import { GHMTableComponent } from '../../components/table/table.component';
+
+import { ApiService } from '../../service/api.service';
+
+import { IPlayerResponse } from '../../types/api';
+
+@Component({
+    selector: 'genieshm-players',
+    standalone: true,
+    imports: [
+        CommonModule,
+        LucideAngularModule,
+        GHMButtonComponent,
+        GHMTableComponent,
+        GHMInputComponent,
+        GHMModalComponent
+    ],
+    templateUrl: './players.page.html',
+    styleUrl: './players.page.css'
+})
+export class PlayersPage {
+    api = inject(ApiService);
+
+    players: Signal<QueryObserverResult<IPlayerResponse[], Error>>;
+
+    teams: any[] = [];
+
+    countries: any[] = [];
+
+    isModalVisible: boolean = false;
+    isEditing: boolean = false;
+
+    playerId: string = '';
+    playerSteamId: string = '';
+    playerNickname: string = '';
+    playerAvatar: string = '';
+    playerFirstName: string = '';
+    playerLastName: string = '';
+    playerCountry: string = '';
+    playerTeam: string = '';
+
+    constructor() {
+        this.players = this.api.getPlayers().result;
+    }
+
+    private resetInputs = () => {
+        this.playerId = '';
+        this.playerSteamId = '';
+        this.playerAvatar = '';
+        this.playerNickname = '';
+        this.playerFirstName = '';
+        this.playerLastName = '';
+        this.playerCountry = '';
+        this.playerTeam = '';
+    };
+
+    openModal = (action: 'create' | 'update' | 'delete', player?: IPlayer) => {
+        this.isModalVisible = action === 'create' || action === 'update';
+
+        switch (action) {
+            case 'create':
+                break;
+            case 'update':
+                this.isEditing = true;
+                this.playerId = player?.id || '';
+                this.playerSteamId = player?.steamId || '';
+                this.playerAvatar = player?.avatar || '';
+                this.playerNickname = player?.nickname || '';
+                this.playerFirstName = player?.firstName || '';
+                this.playerLastName = player?.lastName || '';
+                this.playerCountry = player?.country || '';
+                this.playerTeam = player?.team?.id || '';
+                break;
+            case 'delete':
+                console.log(player);
+                break;
+        }
+    };
+
+    closeModal = () => {
+        this.resetInputs();
+        this.isModalVisible = false;
+        this.isEditing = false;
+    };
+
+    save = () => {
+        this.api.savePlayer(this.isEditing).mutateAsync({
+            id: this.playerId,
+            steamId: this.playerSteamId,
+            avatar: this.playerAvatar,
+            nickname: this.playerNickname,
+            firstName: this.playerFirstName,
+            lastName: this.playerLastName,
+            country: this.playerCountry,
+            teamId: this.playerTeam
+        });
+        this.closeModal();
+    };
+}
