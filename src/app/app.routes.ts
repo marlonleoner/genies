47483749f756b77e97c193a86be01420d0acdbc4
Marlon Leoner <marlon.leoner@gmail.com>
2024-03-16 14:54:43 -0300

    teams and not found pages add to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { NotFoundPage } from './pages/not-found/not-found.page';
+import { TeamsPage } from './pages/teams/teams.page';
+
+export const routes: Routes = [
+    { path: 'teams', component: TeamsPage },
+    { path: '**', component: NotFoundPage }
+];
