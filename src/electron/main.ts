615f380bfdfbec5e09b4d9025932e10d4f7c92f9
Marlon Leoner <marlon.leoner@gmail.com>
2024-03-23 13:48:48 -0300

    main electron updated

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,91 +1,84 @@
-import { app, BrowserWindow, ipcMain } from 'electron';
+import { BrowserWindow, app, ipcMain } from 'electron';
 
 import '../api/';
 
-import { GlobalEmitter } from '../api/lib/emitter';
+var window: BrowserWindow | null = null;
 
-const receive = (event: string, cb: (...arg: any) => void) => {
-    console.log(`configurando emitter: ${event} - ${cb}`);
+var hudWindow: BrowserWindow | null = null;
+
+const createWindow = () => {
+    window = new BrowserWindow({
+        width: 800,
+        minWidth: 1280,
+        height: 600,
+        minHeight: 960,
+        center: true,
+        frame: false,
+        autoHideMenuBar: true,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+            devTools: true
+        }
+    });
+    window.on('close', () => {
+        window = null;
+        if (hudWindow) {
+            hudWindow.close();
+            hudWindow = null;
+        }
+    });
+    window.loadURL('http://localhost:6779');
+    window.webContents.setVisualZoomLevelLimits(1, 1);
+
+    ipcMain.on('@GHM/CLOSE_APP', () => {
+        window?.close();
+    });
+
+    ipcMain.on('@GHM/MAXIMIZE_APP', () => {
+        if (!window) return;
+        if (window.isMaximized()) window.restore();
+        else window.maximize();
+    });
+
+    ipcMain.on('@GHM/MINIMIZE_APP', () => {
+        window?.minimize();
+    });
+
+    ipcMain.on('open-hud', async (event: any, hudName: string) => {
+        if (hudWindow || !hudName) return;
+
+        const hudURL = hudName === 'development' ? 'http://localhost:4200' : 'https://localhost:6779';
+
+        hudWindow = new BrowserWindow({
+            movable: false,
+            resizable: false,
+            fullscreen: true,
+            alwaysOnTop: true,
+            titleBarStyle: 'hidden',
+            transparent: true
+        });
+        hudWindow.on('close', () => {
+            hudWindow = null;
+        });
+        hudWindow.loadURL(hudURL);
+        hudWindow.setIgnoreMouseEvents(true);
+        hudWindow.show();
+    });
 };
 
-class Main {
-    private window!: BrowserWindow;
-
-    private hudWindow: BrowserWindow | null = null;
-
-    public init() {
-        app.on('ready', this.createWindow);
-        app.on('window-all-closed', this.onWindowAllClosed);
-        app.on('activate', this.onActivate);
-
-        ipcMain.on('open-hud', async (e) => {
-            if (!this.hudWindow) {
-                this.hudWindow = new BrowserWindow({
-                    movable: false,
-                    resizable: false,
-                    fullscreen: true,
-                    alwaysOnTop: true,
-                    titleBarStyle: 'hidden',
-                    transparent: true
-                });
-
-                this.hudWindow.webContents.executeJavaScript(`
-                    window.ipc = {
-                        send: function(event, data) {
-                            // let ipc = windown.require('electron').ipcRenderer;
-                            // ipc.send(event, data);
-                            console.log('send', event, data);
-                        },
-                        receive: ${receive}
-                    };
-                `);
-
-                this.hudWindow.loadURL('http://localhost:4200');
-                this.hudWindow.setIgnoreMouseEvents(true);
-                this.hudWindow.on('close', () => {
-                    this.hudWindow = null;
-                });
-
-                this.hudWindow.show();
-            }
-        });
-
-        GlobalEmitter.on('raw', (raw) => {
-            console.log('raw', raw);
-        });
+const onWindowAllClosed = () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
     }
+};
 
-    private onWindowAllClosed() {
-        if (process.platform !== 'darwin') {
-            this.hudWindow?.close();
-            app.quit();
-        }
+const onActivate = () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
     }
+};
 
-    private onActivate() {
-        if (!this.window) {
-            this.createWindow();
-        }
-    }
-
-    private createWindow() {
-        this.window = new BrowserWindow({
-            width: 800,
-            minWidth: 1280,
-            height: 600,
-            minHeight: 960,
-            frame: false,
-            autoHideMenuBar: true,
-            webPreferences: {
-                nodeIntegration: true,
-                contextIsolation: false,
-                devTools: true
-            }
-        });
-        this.window.loadURL('http://localhost:6779');
-        this.window.maximize();
-        this.window.webContents.setVisualZoomLevelLimits(1, 1);
-    }
-}
-
-new Main().init();
+app.on('ready', createWindow);
+app.on('window-all-closed', onWindowAllClosed);
+app.on('activate', onActivate);
